62ad8fbce91772828457819067f2af7d0aef57d4
slybootslion <slybootslion@vip.qq.com>
2022-10-13 16:58:11 +0800

    场景、相机、渲染器

--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import * as THREE from 'three'
 
 const SceneJsx = () => {
-
   const init = () => {
     const container = document.querySelector(".container");
 
